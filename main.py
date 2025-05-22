4599444f886edd0c8a9b40c1a0c62efa67fb0588
Yang Leng <yang.leng@fapongroup.com>
2025-05-22 07:52:52 +0000

    not allow domains cross-origin

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.api import router
 
 app = FastAPI()
-#app.include_router(router)
 
-# 允许所有域名跨域（开发环境推荐，生产环境建议指定域名）
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # 允许所有前端域名
-    allow_credentials=True,
-    allow_methods=["*"],  # 允许所有方法
-    allow_headers=["*"],  # 允许所有请求头
-)
+# allow all domains cross-origin (recommended for development environment, specify domains for production environment)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],  # allow all frontend domains
+#     allow_credentials=True,
+#     allow_methods=["*"],  # allow all methods
+#     allow_headers=["*"],  # allow all request headers
+# )
 
 app.include_router(router)
 
